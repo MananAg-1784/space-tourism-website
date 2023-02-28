a7d422790be666bf4eadedab0c2e60d55ae0195c
Itachi-o-o <mananagarwal1784@gmail.com>
2023-02-28 20:31:07 +0530

    initital commit
    Mobile responsive yet to be done

---
+++ anchor_link.js
@@ -0,0 +1,22 @@
+       
+/*Get the pathname of the current file
+const activePage = window.location.pathname;
+console.log(activePage);
+
+const navLinks = document.querySelectorAll( 'a' ).
+ forEach( link => {
+    if( link.href.includes('${activePage}')) {
+        link.classList.add('active');
+    }
+ }) */
+       
+       
+       
+/* $(document).ready( function(){
+
+    $('a').on('click', function(){
+        var target = $(this.getAttribute('href'));
+        target.addClass('active');
+    });
+
+}); */
