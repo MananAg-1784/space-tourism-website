87e6e8a7bcd95085db861653a912f82560fc7c99
Itachi-o-o <mananagarwal1784@gmail.com>
2023-03-02 00:54:11 +0530

    made responsive nav bar for mobile

--- java_script.js
+++ java_script.js
@@ -1,4 +1,24 @@
-       
+
+$(document).ready( function(){
+    var img_1 = $('#nav_mobile_icon img:first-child');
+    var img_2 = $('#nav_mobile_icon img:last-child');
+
+    img_1.on('click', function(){
+        img_2.css('display','inline');
+        img_1.css('display','none');
+        $('#nav-menu').toggle('slide');
+    });
+
+    img_2.on('click', function(){
+        $('#nav-menu').toggle('slide');
+        img_1.css('display','inline');
+        img_2.css('display','none');
+    });
+
+});
+
+
+
 /*Get the pathname of the current file
 const activePage = window.location.pathname;
 console.log(activePage);
